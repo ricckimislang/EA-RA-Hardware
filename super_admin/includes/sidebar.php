248d19c1b22d2470ef60f078ae6d8eb81e2d9373
riccki mislang <dragonmislang9@gmail.com>
2025-04-28 13:13:49 +0800

    fixed sidebar for admin and superadmin

--- super_admin/includes/sidebar.php
+++ super_admin/includes/sidebar.php
@@ -5,20 +5,84 @@
     </div>
 
     <ul class="sidebar-menu">
+        <?php if ($_SESSION['usertype'] == '1') { ?>
+            <li>
+                <a href="http://localhost/ea-ra-hardware/admin/pages/dashboard.php" class="<?php echo (basename($_SERVER['PHP_SELF']) == 'dashboard.php') ? 'active' : ''; ?>">
+                    <i class="fas fa-tachometer-alt"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+
+            <li>
+                <a href="http://localhost/ea-ra-hardware/super_admin/pages/payroll.php" class="<?php echo (strpos($_SERVER['PHP_SELF'], 'payroll')) ? 'active' : ''; ?>">
+                    <i class="fas fa-wallet"></i>
+                    <span>Payroll</span>
+                </a>
+            </li>
+
+
+            <li>
+                <a href="http://localhost/ea-ra-hardware/admin/pages/inventory.php" class="<?php echo (strpos($_SERVER['PHP_SELF'], 'inventory')) ? 'active' : ''; ?>">
+                    <i class="fas fa-boxes"></i>
+                    <span>Inventory</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="http://localhost/ea-ra-hardware/admin/pages/employees.php" class="<?php echo (strpos($_SERVER['PHP_SELF'], 'employees')) ? 'active' : ''; ?>">
+                    <i class="fas fa-users"></i>
+                    <span>Employees</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="http://localhost/ea-ra-hardware/admin/pages/expenses.php" class="<?php echo (strpos($_SERVER['PHP_SELF'], 'expenses')) ? 'active' : ''; ?>">
+                    <i class="fas fa-file-invoice-dollar"></i>
+                    <span>Expenses</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="http://localhost/ea-ra-hardware/admin/pages/attendance_reports.php" class="<?php echo (strpos($_SERVER['PHP_SELF'], 'attendance_reports')) ? 'active' : ''; ?>">
+                    <i class="fas fa-clock"></i>
+                    <span>Attendance Reports</span>
+                </a>
+            </li>
+            <li>
+                <a href="http://localhost/ea-ra-hardware/admin/pages/employee_qr.php" class="<?php echo (strpos($_SERVER['PHP_SELF'], 'employee_qr')) ? 'active' : ''; ?>">
+                    <i class="fas fa-clock"></i>
+                    <span>Employee Qr Codes</span>
+                </a>
+            </li>
+        <?php } else if ($_SESSION['usertype'] == '2') { ?>
+            <li>
+                <a href="http://localhost/ea-ra-hardware/admin/pages/inventory.php" class="<?php echo (strpos($_SERVER['PHP_SELF'], 'inventory')) ? 'active' : ''; ?>">
+                    <i class="fas fa-boxes"></i>
+                    <span>Inventory</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="http://localhost/ea-ra-hardware/admin/pages/expenses.php" class="<?php echo (strpos($_SERVER['PHP_SELF'], 'expenses')) ? 'active' : ''; ?>">
+                    <i class="fas fa-file-invoice-dollar"></i>
+                    <span>Expenses</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="http://localhost/ea-ra-hardware/admin/pages/attendance_reports.php" class="<?php echo (strpos($_SERVER['PHP_SELF'], 'attendance_reports')) ? 'active' : ''; ?>">
+                    <i class="fas fa-clock"></i>
+                    <span>Attendance Reports</span>
+                </a>
+            </li>
+        <?php } ?>
         <li>
-            <a href="../pages/dashboard.php" class="<?php echo (basename($_SERVER['PHP_SELF']) == 'dashboard.php') ? 'active' : ''; ?>">
-                <i class="fas fa-tachometer-alt"></i>
-                <span>Dashboard</span>
+            <a href="http://localhost/ea-ra-hardware/admin/pages/settings.php" class="<?php echo (strpos($_SERVER['PHP_SELF'], 'settings')) ? 'active' : ''; ?>">
+                <i class="fas fa-cog"></i>
+                <span>Settings</span>
             </a>
         </li>
-
-        <li>
-            <a href="../pages/payroll.php" class="<?php echo (strpos($_SERVER['PHP_SELF'], 'payroll')) ? 'active' : ''; ?>">
-                <i class="fas fa-wallet"></i>
-                <span>Payroll</span>
-            </a>
-        </li>
-
 
         <li>
             <a href="logout.php">
